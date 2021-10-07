91b334a54eb0999b83a5229048f071b2a205952f
Emre Seyhan <emreseyhan05@hotmail.com>
2021-10-07 20:44:16 +0300

    lesson-2

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
- 
+
+
+//Template Engine
+app.set("view engine", "ejs")
+
+//middle wares
+app.use(express.static("public"))
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.render('index', {
+        page_name: "index"
+    })
 })
 
-
+app.get('/about', function (req, res) {
+    res.render('about', {
+        page_name: "about"
+    })
+})
 
 const port = 3000
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Sunucu ${port} portunda yayında`)
 })
